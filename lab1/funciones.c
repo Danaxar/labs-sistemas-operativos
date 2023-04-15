81595405e7774bf5e7a558fa38d917e2367dad07
= <=>
2023-04-15 18:58:59 -0400

    feat: processing

---
+++ lab1/funciones.c
@@ -0,0 +1,78 @@
+#include "funciones.h"
+
+// Obtiene la cantidad de lineas del nombre del archivo después del separador "_"
+// Ej: prueba_10.txt -> 10, prueba_100.txt = 100
+int getCantidadLineas(char* nombreArchivo){
+    char* posicionSeparador = strchr(nombreArchivo, '_') + 1;
+    return atoi(posicionSeparador);
+}
+
+// Entrada: nombre del archivo a leer con extensión
+// Salida: Matriz de cadena de caracteres del contenido del archivo
+// Lee un archivo de principio a fin
+char** leerPrueba(char* nombreArchivo, int* cantidadLineas){
+    int lenLinea = 59;
+
+    FILE* archivo = fopen(nombreArchivo, "r");
+    if(archivo == NULL){
+        printf("No se ha podido leer el archivo\n");
+        exit(1);
+    }
+
+    *cantidadLineas = getCantidadLineas(nombreArchivo);
+    printf("Leyendo: %s    Lineas: %d\n", nombreArchivo, *cantidadLineas);
+
+    // Leer
+    char** salida = (char**) malloc(sizeof(char*) * *cantidadLineas);
+    for(int i = 0; i < *cantidadLineas; i++){
+        salida[i] = (char*) malloc(sizeof(char) * lenLinea);
+        fscanf(archivo, "%s", salida[i]);
+    }
+
+    fclose(archivo);
+    return salida;
+}
+
+void estado1(int* estadoActual, char* expresion, int indiceActual){
+    if(indiceActual == 59){
+        return;
+    }
+    *estadoActual = 1;
+    if(expresion[indiceActual] == 'A' || expresion[indiceActual] == 'C' || expresion[indiceActual] == 'T'){
+        estado1(estadoActual, expresion, indiceActual + 1);
+    }else if(expresion[indiceActual] == 'G'){
+        estado2(estadoActual, expresion, indiceActual + 1);
+    }
+}
+void estado2(int* estadoActual, char* expresion, int indiceActual){
+    if(indiceActual == 59){
+        return;
+    }
+    *estadoActual = 2;
+    if(expresion[indiceActual] == 'A' || expresion[indiceActual] == 'C' || expresion[indiceActual] == 'G'){
+        estado1(estadoActual, expresion, indiceActual + 1);
+    }else if(expresion[indiceActual] == 'T'){
+        estado3(estadoActual, expresion, indiceActual + 1);
+    }
+}
+void estado3(int* estadoActual, char* expresion, int indiceActual){
+    if(indiceActual == 59){
+        return;
+    }
+    *estadoActual = 3;
+    if(expresion[indiceActual] == 'A' || expresion[indiceActual] == 'G'){
+        estado1(estadoActual, expresion, indiceActual + 1);
+    }else if(expresion[indiceActual] == 'C'){
+        estado4(estadoActual, expresion, indiceActual + 1);
+    }else if(expresion[indiceActual] == 'T'){
+        estado3(estadoActual, expresion, indiceActual + 1);
+    }
+}
+
+void estado4(int* estadoActual, char* expresion, int indiceActual){
+    if(indiceActual == 59){
+        return;
+    }
+    *estadoActual = 4;
+    estado4(estadoActual, expresion, indiceActual + 1);
+}
--- lab1/lab1.c
+++ lab1/lab1.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <string.h>
+#include "funciones.h"
 #define false 0
 #define true 1
 #define boolean int
@@ -47,12 +48,34 @@ int main(int argc, char *argv[]){
             break;
         }
     }
-
     printf("Entradas del sistema:\n");
     printf("\ti: %s\n\to: %s\n", i, o);
     printf("\tb: %d\n", b);
+    // En este punto ya se tienen las entradas listas
+
+    // Procesamiento
+    int cantidadLineas;
+    char** contenido = leerPrueba(i, &cantidadLineas);
+    for(int i = 0; i < cantidadLineas; i++){
+        printf("Linea (%d): %s\n", i+1, contenido[i]);
+        int estado = 1;
+        estado1(&estado, contenido[i], 0);
+        printf("Estado resultante: %d -> ", estado);
+        if(estado == 4){
+            printf("SI");
+        }else{
+            printf("NO");
+        }
+        printf("\n\n\n");
+    }
+
+    // Salida
     free(i);
     free(o);
+    for(int i = 0; i < cantidadLineas; i++){
+        free(contenido[i]);
+    }
+    free(contenido);
     printf("Finalizando...\n");
     return 0;
 }
